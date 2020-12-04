eb4b29970749aaa9770cd045e11df1641fa17595
OhmyGoditsJasonBourne <72467379+OhmyGoditsJasonBourne@users.noreply.github.com>
2020-12-04 22:40:27 +0100

    added Messages extends Notification

--- app/src/main/java/main/modell/data/Notification.java
+++ app/src/main/java/main/modell/data/Notification.java
@@ -1,4 +1,12 @@
 package main.modell.data;
 
 public class Notification {
+
+    int ID;
+    String name;
+
+    public Notification(int ID, String name){
+        this.ID = ID;
+        this.name = name;
+    }
 }
