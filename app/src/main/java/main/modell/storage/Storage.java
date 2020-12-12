f807f0d1d116b5106336cebf215657bd59e28053
Ktechen <kevHTW@gmx.de>
2020-12-12 19:58:47 +0100

    Add Storage

--- app/src/main/java/main/modell/storage/Storage.java
+++ app/src/main/java/main/modell/storage/Storage.java
@@ -1,8 +1,40 @@
 package main.modell.storage;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import main.modell.data.IChannel;
+import main.modell.data.INotification;
+import main.modell.data.IUser;
+
 class Storage {
 
+    private List<IUser> userList;
+    private List<IChannel> channelList;
+    private List<INotification> notificationList;
 
+    public Storage() {
+        this.userList = new LinkedList<>();
+        this.channelList = new LinkedList<>();
+        this.notificationList = new ArrayList<>();
+    }
 
+    public void addUser(IUser iUser) {
+        this.userList.add(iUser);
+    }
 
+    public void addChannelList(IChannel iChannel) {
+        this.channelList.add(iChannel);
+    }
+
+    public void addNotification(INotification iNotification) {
+        this.notificationList.add(iNotification);
+    }
+
+    public void clear() {
+        this.userList = null;
+        this.channelList = null;
+        this.notificationList = null;
+    }
 }
