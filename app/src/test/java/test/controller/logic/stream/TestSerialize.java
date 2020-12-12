0e5e710a458862df0c4e9741825027aae310f0e4
Ktechen <kevHTW@gmx.de>
2020-12-12 19:30:21 +0100

    add Serializable

---
+++ app/src/test/java/test/controller/logic/stream/TestSerialize.java
@@ -0,0 +1,45 @@
+package test.controller.logic.stream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+import main.controller.logic.stream.Serialize;
+import main.modell.data.IUser;
+import main.modell.data.User;
+
+public class TestSerialize {
+
+    private static String iUserName = "Kevin";
+    private static String iUserID = "123";
+    private static String iUser1Name = "Matis";
+    private static String iUser1ID = "3242";
+
+    private static IUser iUser = new User(iUserName, iUserID);
+    private static IUser iUser1 = new User(iUser1Name, iUser1ID);
+
+
+    @Test
+    public void serializeAndDeserialize() throws IOException {
+
+        String msg = "Ich Sende jetzt los alter";
+        String msg1 = "Ich sende jetzt zurück alter";
+
+        Serialize serialize = new Serialize();
+        byte[] one = serialize.serializer(iUser, msg);
+        byte[] two = serialize.serializer(iUser1, msg1);
+        DataInputStream data = serialize.deserializer(one);
+        DataInputStream data1 = serialize.deserializer(two);
+
+        Assert.assertEquals(iUserName, data.readUTF());
+        Assert.assertEquals(iUserID, data.readUTF());
+        Assert.assertEquals(msg, data.readUTF());
+
+        Assert.assertEquals(iUser1Name, data1.readUTF());
+        Assert.assertEquals(iUser1ID, data1.readUTF());
+        Assert.assertEquals(msg1, data1.readUTF());
+    }
+
+}
