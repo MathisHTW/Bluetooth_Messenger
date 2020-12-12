4989e3ebd99b49802bfe8faf3b1498c23793a368
Ktechen <kevHTW@gmx.de>
2020-12-12 19:53:47 +0100

    add gui

---
+++ app/src/main/java/main/CreateChannel.java
@@ -0,0 +1,15 @@
+package main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateChannel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_channel);
+    }
+
+
+}
--- app/src/main/java/main/Start.java
+++ app/src/main/java/main/Start.java
@@ -1,6 +1,9 @@
 package main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +21,26 @@ public class Start extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        final Intent channel = new Intent(this, Channel.class);
+        final Intent create = new Intent(this, CreateChannel.class);
+        Button buttonChannel = findViewById(R.id.btnJoin);
+        Button buttonCreate = findViewById(R.id.btnCreateChannel);
+
+        buttonChannel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(channel);
+            }
+        });
+
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(create);
+            }
+        });
+
         new ASAPInit();
     }
 }
