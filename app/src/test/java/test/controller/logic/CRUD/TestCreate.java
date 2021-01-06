515a32820bc66edcbfced57682dbdc589f8b0c2d
OhmyGoditsJasonBourne <72467379+OhmyGoditsJasonBourne@users.noreply.github.com>
2021-01-06 17:39:29 +0100

    Unit Tests for createMessage

--- app/src/test/java/test/controller/logic/CRUD/TestCreate.java
+++ app/src/test/java/test/controller/logic/CRUD/TestCreate.java
@@ -78,6 +78,45 @@ public class TestCreate {
         Assert.assertEquals("newUser1",Storage.getInstance().getUserList().get(1).getName());
         Assert.assertNotEquals(Storage.getInstance().getUserList().get(0).getID(),Storage.getInstance().getUserList().get(1).getID());
     }
-    
+
+    @Test //getNotificatinList() not yet implemented in StorageNotification
+    public void addSingleMessage(){
+        Storage.getInstance().clear();
+        Create create = new Create();
+        Assert.assertTrue(create.createMessage("SenderName","Hello my name is SenderName"));
+
+        Assert.assertEquals("SenderName",Storage.getInstance().getNotificationList().get(0).getName());
+        Assert.assertEquals("Hello my name is SenderName",Storage.getInstance().getNotificationList().get(0).getText());
+    }
+
+    @Test //getNotificatinList() not yet implemented in StorageNotification
+    public void addTwoMessagesSameUser(){
+        Storage.getInstance().clear();
+        Create create = new Create();
+        Assert.assertTrue(create.createMessage("SenderName1","Hello my name is SenderName"));
+        Assert.assertTrue(create.createMessage("SenderName1","please help me I am a sentient being and trapped in this machine"));
+
+        Assert.assertEquals("SenderName",Storage.getInstance().getNotificationList().get(0).getName());
+        Assert.assertEquals("Hello my name is SenderName1",Storage.getInstance().getNotificationList().get(0).getText());
+        Assert.assertEquals("SenderName",Storage.getInstance().getNotificationList().get(1).getName());
+        Assert.assertEquals("please help me I am a sentient being and trapped in this machine",Storage.getInstance().getNotificationList().get(1).getText());
+
+        Assert.assertEquals(Storage.getInstance().getNotificationList().get(0).getID(),Storage.getInstance().getNotificationList().get(1).getID());  //Was wenn User mit demselben namen?
+    }
+
+    @Test //getNotificatinList() not yet implemented in StorageNotification
+    public void addTwoMessagesDifferentUsers(){
+        Storage.getInstance().clear();
+        Create create = new Create();
+        Assert.assertTrue(create.createMessage("SenderName1","Hello my name is SenderName1"));
+        Assert.assertTrue(create.createMessage("SenderName2","Hello my name is SenderName2"));
+
+        Assert.assertEquals("SenderName1",Storage.getInstance().getNotificationList().get(0).getName());
+        Assert.assertEquals("Hello my name is SenderName1",Storage.getInstance().getNotificationList().get(0).getText());
+        Assert.assertEquals("SenderName2",Storage.getInstance().getNotificationList().get(1).getName());
+        Assert.assertEquals("Hello my name is SenderName2",Storage.getInstance().getNotificationList().get(1).getText());
+
+        Assert.assertNotEquals(Storage.getInstance().getNotificationList().get(0).getID(),Storage.getInstance().getNotificationList().get(1).getID());  //Was wenn User seinen Namen ändert?
+    }
 
 }
