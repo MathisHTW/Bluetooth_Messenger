32f3473273f3732f1c91085d8c2958d0496542bf
OhmyGoditsJasonBourne <72467379+OhmyGoditsJasonBourne@users.noreply.github.com>
2021-01-09 12:31:15 +0100

    Unit Tests for getChannels() und getAppOwner()

--- app/src/test/java/test/controller/logic/CRUD/TestRead.java
+++ app/src/test/java/test/controller/logic/CRUD/TestRead.java
@@ -3,7 +3,9 @@ package test.controller.logic.CRUD;
 import org.junit.Assert;
 import org.junit.Test;
 
+import main.controller.logic.CRUD.Create;
 import main.controller.logic.CRUD.Read;
+import main.modell.storage.Storage;
 
 public class TestRead {
 
@@ -11,6 +13,28 @@ public class TestRead {
     public void defaultSizeOfChannelIsOne() {
         final Read read = new Read();
         Assert.assertEquals(1, read.getChannels().size());
+    }
+
+    @Test
+    public void getChannels(){
+        Storage.getInstance().clear();
+        Create create = new Create();
+        create.createChannel("Power");
+        create.createChannel("Power2");
+
+        Read read = new Read();
+        Assert.assertEquals("Power",read.getChannels().get(1).getName());   //first ChannelName = Public
+        Assert.assertEquals("Power2",read.getChannels().get(2).getName());
+    }
+
+    @Test
+    public void getAppOwner(){
+        Storage.getInstance().clear();
+        Create create = new Create();
+        create.createUser("AppOwner");
+
+        Read read = new Read();
+        Assert.assertEquals("AppOwner",read.getAppOwner().getName());
     }
 
 
