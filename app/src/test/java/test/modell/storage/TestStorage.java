c61332577b5e11fc6918001303d82a00657cb97f
Ktechen <kevHTW@gmx.de>
2021-01-11 14:58:39 +0100

    Add new Unit test

--- app/src/test/java/test/modell/storage/TestStorage.java
+++ app/src/test/java/test/modell/storage/TestStorage.java
@@ -8,13 +8,13 @@ import main.modell.storage.Storage;
 public class TestStorage {
 
     @Test
-    public void clear(){
+    public void clear() {
         Storage.getInstance().clear();
         Assert.assertEquals(1, Storage.getInstance().getChannelList().size());
     }
 
     @Test
-    public void clearOnlyOnePublicChannel(){
+    public void clearOnlyOnePublicChannel() {
         Storage.getInstance().clear();
         Storage.getInstance().clear();
         Storage.getInstance().clear();
@@ -23,11 +23,21 @@ public class TestStorage {
     }
 
     @Test
-    public void getStorage(){
+    public void getStorage() {
+        Storage.getInstance().clear();
         Storage.getInstance().getChannelList();
         Storage.getInstance().getChannelList();
         Storage.getInstance().getChannelList();
         Storage.getInstance().getChannelList();
         Assert.assertEquals(1, Storage.getInstance().getChannelList().size());
+    }
+
+    @Test
+    public void getStorageDefault() {
+        Storage.getInstance().clear();
+        Assert.assertEquals(1, Storage.getInstance().getChannelList().size());
+        Assert.assertEquals(0, Storage.getInstance().getUserList().size());
+        Assert.assertEquals(0, Storage.getInstance().getNotificationList().size());
+        Assert.assertEquals(Storage.DEFAULT_APP_USERNAME, Storage.getInstance().getAppOwnerName().getName());
     }
 }
